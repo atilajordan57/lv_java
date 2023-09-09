c3599a56e3d4a697f623a5e47995f40983bcf1d8
atila <atila@10.0.0.109>
2023-09-09 11:33:27 -0400

    commit inicial LV em Java

---
+++ src/main/java/lv/com/virtual/LvJavaApplication.java
@@ -0,0 +1,13 @@
+package lv.com.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/com/virtual/LvJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.com.virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
