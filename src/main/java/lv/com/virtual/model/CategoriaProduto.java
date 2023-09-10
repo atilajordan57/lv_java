2d8a640f17acb455b3e16127d3b141f37b9cd6ac
atila <atila@10.0.0.109>
2023-09-10 00:16:32 -0400

    criada entidade categoriaProduto

--- src/main/java/lv/com/virtual/model/CategoriaProduto.java
+++ src/main/java/lv/com/virtual/model/CategoriaProduto.java
@@ -11,18 +11,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
+
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable{
-	
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	
-	@Column(name = "nome_desricao_p", nullable = false)
+	@Column(name = "nome_desricao", nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
@@ -31,6 +32,10 @@ public class MarcaProduto implements Serializable{
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
 	}
 
 	@Override
@@ -46,12 +51,8 @@ public class MarcaProduto implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	public String getNomeDesc() {
-		return nomeDesc;
 	}
 
 	public void setNomeDesc(String nomeDesc) {
@@ -59,5 +60,4 @@ public class MarcaProduto implements Serializable{
 	}
 	
 	
-
 }
